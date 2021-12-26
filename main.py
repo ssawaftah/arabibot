ef679282a1079cdf0d4b09fd3c638bd0c70695c1
ssawaftah <74429413+ssawaftah@users.noreply.github.com>
2021-12-26 21:15:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -108,8 +108,8 @@ def tiktok_dl(client, message):
                     if percent == 100:
                         show = 0
 
-        a.edit(f'__يرجى الإنتظار'
-               f' تمت معالجة الفيديو__')
+        a.edit(f'__تمت معالجة'
+               f'  الفيديو__')
         start = time.time()
         title = filename
         app.send_document(chat_id=message.chat.id,
